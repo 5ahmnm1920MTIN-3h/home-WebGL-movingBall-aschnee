757609efd170db263fef93948c54e2f7f655f070
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2020-01-13 15:41:24 +0100

    added canvas

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    [SerializeField] private Transform[] randomPositions;
+    [SerializeField] private Transform defaultPosition;
+    [SerializeField] private Transform ball;
+    [SerializeField] private bool alwaysDifferentPos;
+
+    void Start()
+    {
+        ball.position = defaultPosition.position;
+    }
+
+    public void BallMover()
+    {
+        Vector3 oldPos;
+        Vector3 newPos;
+
+        do
+        {
+            oldPos = ball.position;
+            newPos = randomPositions[Random.Range(0, randomPositions.Length)].position;
+            ball.position = newPos;
+        }
+        // check condition after executing the above code
+        while (oldPos == newPos && alwaysDifferentPos);
+    }
+
+    public void ChangeMode(bool setting)
+    {
+        alwaysDifferentPos = setting;
+    }
+}
